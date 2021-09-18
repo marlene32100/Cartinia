281da10bfb0b1fa7078fa10ae65aab9eb836683e
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-09-18 11:47:15 +0200

    Added content to pages from backend

--- app.js
+++ app.js
@@ -56,13 +56,21 @@ app.get("/", async (req, res) => {
           "navbar",
           "homedown",
           "navigation",
+          "footer",
         ])
       )
       .then((response) => {
         const { results } = response;
-        const [home, meta, navbar, homedown, navigation] = results;
-        console.log(navigation.data.link[0]);
-        res.render("pages/home", { home, meta, navbar, homedown, navigation });
+        const [home, meta, navbar, homedown, navigation, footer] = results;
+        console.log(footer.data.social_icons[0].icon);
+        res.render("pages/home", {
+          home,
+          meta,
+          navbar,
+          homedown,
+          navigation,
+          footer,
+        });
       });
   });
 });
@@ -80,29 +88,23 @@ app.get("/team", async (req, res) => {
 });
 
 app.get("/on-sale", async (req, res) => {
-  initApi(req).then((api) => {
-    api
-      .query(Prismic.Predicates.any("document.type", ["on_sale", "meta"]))
-      .then((response) => {
-        const { results } = response;
-        const [on_sale, meta] = results;
-        console.log(on_sale.data.body[0].primary);
-        res.render("pages/on-sale", { on_sale, meta });
-      });
-  });
+  const api = await initApi(req);
+  const on_sale = await api.getSingle("on_sale");
+  const meta = await api.getSingle("meta");
+  const navbar = await api.getSingle("navbar");
+  const navigation = await api.getSingle("navigation");
+  const footer = await api.getSingle("footer");
+  res.render("pages/on-sale", { on_sale, meta, navbar, navigation, footer });
 });
 
 app.get("/sold", async (req, res) => {
-  initApi(req).then((api) => {
-    api
-      .query(Prismic.Predicates.any("document.type", ["sold", "meta"]))
-      .then((response) => {
-        const { results } = response;
-        const [meta, sold] = results;
-        console.log(sold.data.cars[0]);
-        res.render("pages/sold", { meta, sold });
-      });
-  });
+  const api = await initApi(req);
+  const sold = await api.getSingle("sold");
+  const meta = await api.getSingle("meta");
+  const navbar = await api.getSingle("navbar");
+  const navigation = await api.getSingle("navigation");
+  const footer = await api.getSingle("footer");
+  res.render("pages/sold", { sold, meta, navbar, navigation, footer });
 });
 
 app.get("/contact", async (req, res) => {
